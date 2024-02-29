3d9b713dcb67b121d1ecb00687b71e35662238a9
Arthur Vitor <arthurvitorgn@gmail.com>
2024-02-29 17:07:46 -0300

    Service Restaurant

--- backend/src/main/java/com/db/backend/controller/RestaurantController.java
+++ backend/src/main/java/com/db/backend/controller/RestaurantController.java
@@ -1,49 +1,31 @@
 package com.db.backend.controller;
 
-import com.db.backend.dto.AdressDTO;
 import com.db.backend.dto.RestaurantDTO;
-import com.db.backend.entity.Adress;
 import com.db.backend.entity.Restaurant;
-import com.db.backend.infra.security.JwtService;
-import com.db.backend.repository.AdressRepository;
-import com.db.backend.repository.RestaurantRepository;
+import com.db.backend.service.RestaurantService;
 import jakarta.validation.Valid;
-
-import java.util.Collection;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+
 @RestController
 @RequestMapping("restaurant")
 public class RestaurantController {
-
     @Autowired
-    private RestaurantRepository restaurantRepository;
-    @Autowired
-    private AdressRepository adressRepository;
-    @Autowired
-    private JwtService jwtService;
+    private RestaurantService restaurantService;
 
     @PostMapping("/create")
-    public ResponseEntity<Restaurant> createRestaurant(@RequestBody @Valid RestaurantDTO data) {
-        AdressDTO adressDTO = data.adress();
-
-        Adress adress = new Adress(adressDTO.cep(), adressDTO.street(), adressDTO.neighborhood(), adressDTO.locale(),
-                adressDTO.uf());
-        Adress adressSaved = this.adressRepository.save(adress);
-
-        Restaurant restaurant = new Restaurant(data.name().trim(), data.description().trim(), data.website().trim(),
-                adressSaved);
-        this.restaurantRepository.save(restaurant);
-        return new ResponseEntity<>(restaurant, HttpStatus.CREATED);
+    public ResponseEntity<Long> createRestaurant(@RequestBody @Valid RestaurantDTO data) {
+        Long restaurantId = this.restaurantService.saveRestaurant(data);
+        return new ResponseEntity<>(restaurantId, HttpStatus.CREATED);
     }
 
     @GetMapping("/getAll")
     public ResponseEntity<Collection<Restaurant>> getAll() {
-        Collection<Restaurant> restaurants = this.restaurantRepository.findAll();
+        Collection<Restaurant> restaurants = this.restaurantService.getAllRestaurants();
         return new ResponseEntity<>(restaurants, HttpStatus.OK);
     }
 }
