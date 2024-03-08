e0aec35e3c2592b851897d3a38af5aab94d299d8
Arthur Vitor <arthur.nascimento@dbserver.com.br>
2024-03-08 17:05:16 -0300

    restaurant service tests

--- backend/src/main/java/com/db/backend/service/VotingService.java
+++ backend/src/main/java/com/db/backend/service/VotingService.java
@@ -9,11 +9,9 @@ import com.db.backend.repository.RestaurantRepository;
 import com.db.backend.repository.UserRepository;
 import com.db.backend.repository.VotingRepository;
 
-import io.micrometer.core.ipc.http.HttpSender.Response;
 import lombok.NonNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
